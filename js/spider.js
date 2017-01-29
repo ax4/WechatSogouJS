82f65df44eb7574a9fa9f678b51f330aff5757a5
ax4 <ax4@catcode.xyz>
2017-01-29 15:55:47 -0700

    fix localStorage.spiderResults overwrite

--- js/spider.js
+++ js/spider.js
@@ -1,54 +1,57 @@
 function spider_start(page) {
-    localStorage.spiderCounter = 1; 
-    localStorage.spiderResults = "[]"; 
+    localStorage.spiderCounter = 1;
+    if (localStorage.spiderResults == null) {
+        localStorage.spiderResults = "[]";
+    }
+
     if (page) {
         localStorage.spiderCounter = page;
     }
-    var result = getArticlesHref(); 
+    var result = getArticlesHref();
     addToLocalResults(result);
     decreaseSpiderCounter();
-    setTimeout(NextPage,1000);
+    setTimeout(NextPage, 1000);
 }
 
 function spider_continue() {
     if (localStorage.spiderCounter == null) {
-        return console.info("No Spider Task"); 
+        return console.info("No Spider Task");
     }
     if (localStorage.spiderCounter == 0) {
         localStorage.removeItem("spiderCounter");
         return console.info("Spider Task Complete!");
     }
-    var result = getArticlesHref(); 
+    var result = getArticlesHref();
     addToLocalResults(result);
     decreaseSpiderCounter();
-    setTimeout(NextPage,1000);
+    setTimeout(NextPage, 1000);
 }
 
 function addToLocalResults(item) {
-    if (localStorage.spiderResults == null){
+    if (localStorage.spiderResults == null) {
         return console.info("error when addToLocalResults(), check localStorage.spiderResults");
     }
 
     var temp = [];
     temp = JSON.parse(localStorage.spiderResults);
-    if (typeof(item)=="string"){
+    if (typeof (item) == "string") {
         temp.push(item);
     }
-    else if (typeof(item)=="object"){
+    else if (typeof (item) == "object") {
         temp = temp.concat(item);
     }
-    else{
-        return console.info("unknow typeof ", item); 
+    else {
+        return console.info("unknow typeof ", item);
     }
 
     localStorage.spiderResults = JSON.stringify(temp);
 }
 
-function decreaseSpiderCounter(){
-    if (localStorage.spiderCounter == null || localStorage.spiderCounter == 0){
-        return console.info("error when decreaseSpiderCounter(), check localStorage.spiderCounter"); 
+function decreaseSpiderCounter() {
+    if (localStorage.spiderCounter == null || localStorage.spiderCounter == 0) {
+        return console.info("error when decreaseSpiderCounter(), check localStorage.spiderCounter");
     }
-    localStorage.spiderCounter -= 1; 
+    localStorage.spiderCounter -= 1;
 }
 
 spider_continue();
