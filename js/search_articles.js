dfe4e53197bbe504b50c8ff7458218d1e594d7c2
ax4 <ax4@catcode.xyz>
2017-01-30 23:33:57 -0700

    change getArticlesHref() && getArticlesAccount() into a more robust way

--- js/search_articles.js
+++ js/search_articles.js
@@ -4,30 +4,44 @@ function NextPage(){
 
 function getArticlesHref(){
     var href = [];
-    href.push(document.getElementsByTagName("a").sogou_vr_11002601_title_0.href); 
-    href.push(document.getElementsByTagName("a").sogou_vr_11002601_title_1.href);
-    href.push(document.getElementsByTagName("a").sogou_vr_11002601_title_2.href);
-    href.push(document.getElementsByTagName("a").sogou_vr_11002601_title_3.href);
-    href.push(document.getElementsByTagName("a").sogou_vr_11002601_title_4.href);
-    href.push(document.getElementsByTagName("a").sogou_vr_11002601_title_5.href);
-    href.push(document.getElementsByTagName("a").sogou_vr_11002601_title_6.href);
-    href.push(document.getElementsByTagName("a").sogou_vr_11002601_title_7.href);
-    href.push(document.getElementsByTagName("a").sogou_vr_11002601_title_8.href);
-    href.push(document.getElementsByTagName("a").sogou_vr_11002601_title_9.href);
+    var a_tags = document.getElementsByTagName("a");
+    var titles = [a_tags.sogou_vr_11002601_title_0,
+                  a_tags.sogou_vr_11002601_title_1,
+                  a_tags.sogou_vr_11002601_title_2,
+                  a_tags.sogou_vr_11002601_title_3,
+                  a_tags.sogou_vr_11002601_title_4,
+                  a_tags.sogou_vr_11002601_title_5,
+                  a_tags.sogou_vr_11002601_title_6,
+                  a_tags.sogou_vr_11002601_title_7,
+                  a_tags.sogou_vr_11002601_title_8,
+                  a_tags.sogou_vr_11002601_title_9];
+    titles.forEach(function(e){
+        if (e){
+            href.push(e.href)
+        }
+    });
+    
     return href;
 }
 
 function getArticlesAccount(){
     var href = [];
-    href.push(document.getElementsByTagName("a").sogou_vr_11002601_account_0.href); 
-    href.push(document.getElementsByTagName("a").sogou_vr_11002601_account_1.href);
-    href.push(document.getElementsByTagName("a").sogou_vr_11002601_account_2.href);
-    href.push(document.getElementsByTagName("a").sogou_vr_11002601_account_3.href);
-    href.push(document.getElementsByTagName("a").sogou_vr_11002601_account_4.href);
-    href.push(document.getElementsByTagName("a").sogou_vr_11002601_account_5.href);
-    href.push(document.getElementsByTagName("a").sogou_vr_11002601_account_6.href);
-    href.push(document.getElementsByTagName("a").sogou_vr_11002601_account_7.href);
-    href.push(document.getElementsByTagName("a").sogou_vr_11002601_account_8.href);
-    href.push(document.getElementsByTagName("a").sogou_vr_11002601_account_9.href);
+    var a_tags = document.getElementsByTagName("a");
+    var titles = [a_tags.sogou_vr_11002601_account_0,
+                  a_tags.sogou_vr_11002601_account_1,
+                  a_tags.sogou_vr_11002601_account_2,
+                  a_tags.sogou_vr_11002601_account_3,
+                  a_tags.sogou_vr_11002601_account_4,
+                  a_tags.sogou_vr_11002601_account_5,
+                  a_tags.sogou_vr_11002601_account_6,
+                  a_tags.sogou_vr_11002601_account_7,
+                  a_tags.sogou_vr_11002601_account_8,
+                  a_tags.sogou_vr_11002601_account_9];
+    titles.forEach(function(e){
+        if (e){
+            href.push(e.href)
+        }
+    });
+    
     return href;
 }
