656f19b6f5b2b95c4952bb13e3f0e584903e3cd5
ax4 <ax4@catcode.xyz>
2017-02-17 04:38:13 -0700

    add code injection for mp.weixin.qq.com

---
+++ ext/src/inject/weixin.js
@@ -0,0 +1,34 @@
+chrome.extension.sendMessage({}, function (response) {
+	var readyStateCheckInterval = setInterval(function () {
+		if (document.readyState === "complete") {
+			clearInterval(readyStateCheckInterval);
+
+			// ----------------------------------------------------------
+			// This part of the script triggers when page is done loading
+			console.log("Hello. This message was sent from scripts/weixin.js");
+			// ----------------------------------------------------------
+ 		}
+	}, 10);
+
+
+	var loadJS = function (url, location) {
+		//url is URL of external file, implementationCode is the code
+		//to be called from the file, location is the location to 
+		//insert the <script> element
+
+		var scriptTag = document.createElement('script');
+		scriptTag.src = url;
+
+
+		var onload = function () {
+			console.log("Success loadJS from", url);
+		}
+		scriptTag.onload = onload;
+		scriptTag.onreadystatechange = onload;
+
+		location.appendChild(scriptTag);
+	};
+
+	//loadJS('https://ax4.github.io/WechatSogouJS/js/search_articles.js', document.body);
+	//loadJS('https://ax4.github.io/WechatSogouJS/js/spider.js', document.body);
+});
