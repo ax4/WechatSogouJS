3a88617144c67e2903aa54cbff06806a8927a00e
ax4 <ax4@catcode.xyz>
2017-02-18 17:51:46 -0700

    add getArticlesTitle and getArticlesSummary

--- js/search_articles.js
+++ js/search_articles.js
@@ -1,47 +1,91 @@
-function NextPage(){
+function NextPage() {
     window.location.href = document.getElementsByTagName("a").sogou_next.href;
 }
 
-function getArticlesHref(){
-    var href = [];
+function getArticlesHref() {
+    var temp = [];
     var a_tags = document.getElementsByTagName("a");
     var titles = [a_tags.sogou_vr_11002601_title_0,
-                  a_tags.sogou_vr_11002601_title_1,
-                  a_tags.sogou_vr_11002601_title_2,
-                  a_tags.sogou_vr_11002601_title_3,
-                  a_tags.sogou_vr_11002601_title_4,
-                  a_tags.sogou_vr_11002601_title_5,
-                  a_tags.sogou_vr_11002601_title_6,
-                  a_tags.sogou_vr_11002601_title_7,
-                  a_tags.sogou_vr_11002601_title_8,
-                  a_tags.sogou_vr_11002601_title_9];
-    titles.forEach(function(e){
-        if (e){
-            href.push(e.href)
+    a_tags.sogou_vr_11002601_title_1,
+    a_tags.sogou_vr_11002601_title_2,
+    a_tags.sogou_vr_11002601_title_3,
+    a_tags.sogou_vr_11002601_title_4,
+    a_tags.sogou_vr_11002601_title_5,
+    a_tags.sogou_vr_11002601_title_6,
+    a_tags.sogou_vr_11002601_title_7,
+    a_tags.sogou_vr_11002601_title_8,
+    a_tags.sogou_vr_11002601_title_9];
+    titles.forEach(function (e) {
+        if (e) {
+            temp.push(e.href)
         }
     });
-    
-    return href;
+
+    return temp;
 }
 
-function getArticlesAccount(){
-    var href = [];
+function getArticlesAccount() {
+    var temp = [];
     var a_tags = document.getElementsByTagName("a");
     var titles = [a_tags.sogou_vr_11002601_account_0,
-                  a_tags.sogou_vr_11002601_account_1,
-                  a_tags.sogou_vr_11002601_account_2,
-                  a_tags.sogou_vr_11002601_account_3,
-                  a_tags.sogou_vr_11002601_account_4,
-                  a_tags.sogou_vr_11002601_account_5,
-                  a_tags.sogou_vr_11002601_account_6,
-                  a_tags.sogou_vr_11002601_account_7,
-                  a_tags.sogou_vr_11002601_account_8,
-                  a_tags.sogou_vr_11002601_account_9];
-    titles.forEach(function(e){
-        if (e){
-            href.push(e.href)
+    a_tags.sogou_vr_11002601_account_1,
+    a_tags.sogou_vr_11002601_account_2,
+    a_tags.sogou_vr_11002601_account_3,
+    a_tags.sogou_vr_11002601_account_4,
+    a_tags.sogou_vr_11002601_account_5,
+    a_tags.sogou_vr_11002601_account_6,
+    a_tags.sogou_vr_11002601_account_7,
+    a_tags.sogou_vr_11002601_account_8,
+    a_tags.sogou_vr_11002601_account_9];
+    titles.forEach(function (e) {
+        if (e) {
+            temp.push(e.href)
         }
     });
-    
-    return href;
+
+    return temp;
+}
+
+function getArticlesTitle() {
+    var temp = [];
+    var a_tags = document.getElementsByTagName("a");
+    var titles = [a_tags.sogou_vr_11002601_title_0,
+    a_tags.sogou_vr_11002601_title_1,
+    a_tags.sogou_vr_11002601_title_2,
+    a_tags.sogou_vr_11002601_title_3,
+    a_tags.sogou_vr_11002601_title_4,
+    a_tags.sogou_vr_11002601_title_5,
+    a_tags.sogou_vr_11002601_title_6,
+    a_tags.sogou_vr_11002601_title_7,
+    a_tags.sogou_vr_11002601_title_8,
+    a_tags.sogou_vr_11002601_title_9];
+    titles.forEach(function (e) {
+        if (e) {
+            temp.push(e.innerHTML)
+        }
+    });
+
+    return temp;
+}
+
+function getArticlesSummary() {
+    var temp = [];
+    var a_tags = document.getElementsByTagName("a");
+    var titles = [a_tags.sogou_vr_11002601_title_0,
+    a_tags.sogou_vr_11002601_summary_1,
+    a_tags.sogou_vr_11002601_summary_2,
+    a_tags.sogou_vr_11002601_summary_3,
+    a_tags.sogou_vr_11002601_summary_4,
+    a_tags.sogou_vr_11002601_summary_5,
+    a_tags.sogou_vr_11002601_summary_6,
+    a_tags.sogou_vr_11002601_summary_7,
+    a_tags.sogou_vr_11002601_summary_8,
+    a_tags.sogou_vr_11002601_summary_9];
+    titles.forEach(function (e) {
+        if (e) {
+            temp.push(e.innerHTML)
+        }
+    });
+
+    return temp;
 }
