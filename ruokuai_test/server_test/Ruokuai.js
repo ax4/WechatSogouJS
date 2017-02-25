1079d69ed520394d29fc9aff1a79f6313402fd66
ax4 <yaoyue718@gmail.com>
2017-02-24 18:56:39 -0700

    Split Ruokuai from server test

---
+++ ruokuai_test/server_test/Ruokuai.js
@@ -0,0 +1,39 @@
+var rest = require('restler'),
+    fs = require('fs'),
+    settings = require('../config.js').settings,
+    Base64File = require('js-base64-file'),
+    shortid = require('shortid');
+
+const path = `${__dirname}/temp/`;
+
+function Captcha(data, cb = function(res){console.log("Received", res);}) {
+    if (data.includes("base64")) {
+        data = data.split(",")[1];
+    }
+    var image = new Base64File;
+    var filename = shortid.generate() + ".jpg"
+
+    image.save(data, path, filename, (err)=>{
+        rest.post('http://api.ruokuai.com/create.json', {
+            multipart: true,
+            data: {
+                'username': settings.username,
+                'password': settings.password,
+                'typeid': '3060',
+                'softid': settings.softid,
+                'softkey': settings.softkey,
+                'image': rest.file("./temp/" + filename, null, fs.statSync("./temp/"+ filename).size, null, 'image/jpg')// base64 code of the image 
+            },
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0',
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        }).on('complete', function (res) {
+            var captcha = JSON.parse(res);
+            console.log('Captcha Encoded.');
+            cb(captcha); 
+        });
+    });
+}
+
+exports.Captcha = Captcha; 
--- ruokuai_test/server_test/test.js
+++ ruokuai_test/server_test/test.js
@@ -1,46 +1,6 @@
-/*
-* 诺快打码 http 接口(上传)，node.js 示例代码 
-* 注意：需要安装restler : npm install restler
-*/
-
-var rest = require('restler'),
-    fs = require('fs'),
-    settings = require('../config.js').settings,
-    Base64File = require('js-base64-file'),
-    shortid = require('shortid');
-
-const path = `${__dirname}/temp/`;
-
-function Ruokuai(data, cb = function(captcha){console.log("Received", captcha);}) {
-    if (data.includes("base64")) {
-        data = data.split(",")[1];
-    }
-    var image = new Base64File;
-    var filename = shortid.generate() + ".jpg"
-
-    image.save(data, path, filename, (err)=>{
-        rest.post('http://api.ruokuai.com/create.json', {
-            multipart: true,
-            data: {
-                'username': settings.username,
-                'password': settings.password,
-                'typeid': '3060',
-                'softid': settings.softid,
-                'softkey': settings.softkey,
-                'image': rest.file("./temp/" + filename, null, fs.statSync("./temp/"+ filename).size, null, 'image/jpg')// base64 code of the image 
-            },
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0',
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        }).on('complete', function (res) {
-            var captcha = JSON.parse(res);
-            console.log('Captcha Encoded.');
-            cb(captcha); 
-        });
-    });
-}
+var Ruokuai = require('./Ruokuai.js');
 
 var data = require('./data.js').data;
-data.forEach(e => { Ruokuai(e);})
+console.log("Sending How Many Data?  ", data.length)
+data.forEach(e => { Ruokuai.Captcha(e);})
 //Ruokuai(data[0]);
