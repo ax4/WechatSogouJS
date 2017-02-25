c3e96b39900c99f33f456f9b804c8e56b095578a
ax4 <yaoyue718@gmail.com>
2017-02-25 07:09:49 -0700

    add api server

---
+++ ruokuai_test/server_test/server.js
@@ -0,0 +1,28 @@
+var fs = require('fs'), 
+    port = process.env.port || 1337,
+    express = require('express'),
+    app = express(),
+    timeout = require('connect-timeout'),
+    Ruokuai = require('./Ruokuai.js');
+
+//timeout http://stackoverflow.com/questions/21708208/express-js-response-timeout
+//app.use(timeout(5000));
+//allow CROS http://enable-cors.org/server_expressjs.html
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    if (!req.timedout) next();
+});
+
+app.get('/', function (req, res) {
+    res.send("helloworld");
+});
+
+app.get('/long', timeout('10s'),function (req, res) {
+    var img = req.query.img;
+    Ruokuai.Captcha(img, (e)=>{res.send(e);});
+});
+
+var server = app.listen(port, function () {
+    console.log("Ruokuai server online");
+});
