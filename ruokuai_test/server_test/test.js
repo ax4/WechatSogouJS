14d9b0c2f43ca5c92ea152476dfb28498899da97
ax4 <yaoyue718@gmail.com>
2017-02-24 18:37:43 -0700

    finish Ruokuai server test

--- ruokuai_test/server_test/test.js
+++ ruokuai_test/server_test/test.js
@@ -6,36 +6,41 @@
 var rest = require('restler'),
     fs = require('fs'),
     settings = require('../config.js').settings,
-    Base64File = require('js-base64-file');
+    Base64File = require('js-base64-file'),
+    shortid = require('shortid');
 
-const path = `${__dirname}/`;
+const path = `${__dirname}/temp/`;
 
-function Ruokuai(data) {
-    if ()
-
+function Ruokuai(data, cb = function(captcha){console.log("Received", captcha);}) {
+    if (data.includes("base64")) {
+        data = data.split(",")[1];
+    }
     var image = new Base64File;
-    image.save(data, path, filename);
+    var filename = shortid.generate() + ".jpg"
 
-
-    rest.post('http://api.ruokuai.com/create.json', {
-        multipart: true,
-        data: {
-            'username': settings.username,
-            'password': settings.password,
-            'typeid': '3060',
-            'softid': settings.softid,
-            'softkey': settings.softkey,
-            'image': rest.file(filename, null, fs.statSync(filename).size, null, 'image/gif')// base64 code of the image 
-        },
-        headers: {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0',
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-    }).on('complete', function (data) {
-        var captcha = JSON.parse(data);
-        console.log('Captcha Encoded.');
-        console.log(captcha);
+    image.save(data, path, filename, (err)=>{
+        rest.post('http://api.ruokuai.com/create.json', {
+            multipart: true,
+            data: {
+                'username': settings.username,
+                'password': settings.password,
+                'typeid': '3060',
+                'softid': settings.softid,
+                'softkey': settings.softkey,
+                'image': rest.file("./temp/" + filename, null, fs.statSync("./temp/"+ filename).size, null, 'image/jpg')// base64 code of the image 
+            },
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:24.0) Gecko/20100101 Firefox/24.0',
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        }).on('complete', function (res) {
+            var captcha = JSON.parse(res);
+            console.log('Captcha Encoded.');
+            cb(captcha); 
+        });
     });
-
 }
 
+var data = require('./data.js').data;
+data.forEach(e => { Ruokuai(e);})
+//Ruokuai(data[0]);
