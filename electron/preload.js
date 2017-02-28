33c131eb4bde0faeef34419190952d15c96adefc
ax4 <yaoyue718@gmail.com>
2017-02-28 03:31:01 -0700

    fix jQuery load (somehow fix, but not all)

--- electron/preload.js
+++ electron/preload.js
@@ -9,10 +9,13 @@ window.onload = function() {
 if (typeof module === 'object') {window.module = module; module = undefined;}
 var script = document.createElement("script");
     script.src = "https://code.jquery.com/jquery-2.1.4.min.js";
-    script.onload = script.onreadystatechange = function() {
-      $(document).ready(function() {
-        $("#lst-ib").val("Hello, World!");
-      });
-    };
-    document.appendChild(script);
+    script.async = false;
+    //script.onload = script.onreadystatechange = function() {
+      //console.log("script loaded, jQuery")
+      //$(document).ready(function() {
+        //$("#lst-ib").val("Hello, World!");
+      //});
+    //};
+    setTimeout(function(){document.head.appendChild(script);},20)
+    
 if (window.module) module = window.module;
