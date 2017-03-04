6aec7148c5a9983b115c6d3edfb939f4354d2d31
ax4 <yaoyue718@gmail.com>
2017-03-03 17:20:03 -0700

    update the query research

--- chrome/js/query_url.js
+++ chrome/js/query_url.js
@@ -1,3 +1,7 @@
 //  type = 1 (articles) / 2 (official subscription account)
 //  query= String     //keyword
-//  usip = String     //search inside the specific official subscription account
+//  usip = String     //search inside the specific official subscription account /no help
+//  &wxid=oIWsFt6dpFnAEl92_oYmalEQAC0E //important!! 
+//http://weixin.sogou.com/weixin?type=2&ie=utf8&query=%E4%BD%A0%E5%A5%BD&tsn=0&ft=null&et=null&interation=null&wxid=oIWsFt6dpFnAEl92_oYmalEQAC0E&usip=%E5%8D%83%E8%AF%BB&from=tool
+
+// &wxid=oIWsFt6dpFnAEl92_oYmalEQAC0E
