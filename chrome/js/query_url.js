3b9ac8d8fcc93a02fe3db0174132d2e92e25185f
ax4 <yaoyue718@gmail.com>
2017-03-03 19:06:14 -0700

    how to make a in account search

--- chrome/js/query_url.js
+++ chrome/js/query_url.js
@@ -5,3 +5,6 @@
 //http://weixin.sogou.com/weixin?type=2&ie=utf8&query=%E4%BD%A0%E5%A5%BD&tsn=0&ft=null&et=null&interation=null&wxid=oIWsFt6dpFnAEl92_oYmalEQAC0E&usip=%E5%8D%83%E8%AF%BB&from=tool
 
 // &wxid=oIWsFt6dpFnAEl92_oYmalEQAC0E
+
+document.getElementsByClassName("s-sea")[0].value = "赛先生"
+document.getElementById("search_enter").click()
