ee331853582164aef207ef3a313544f82a0cbf76
ax4 <yaoyue718@gmail.com>
2017-03-13 12:00:27 +0800

    update the experimental login.js

---
+++ chrome/js/login.js
@@ -0,0 +1,8 @@
+// in the main webpage
+document.getElementById("loginBtn").click()
+
+// in the iframe
+document.getElementById("switcher_plogin").click()
+document.getElementById("u").value = 123
+document.getElementById("p").value = 123
+document.getElementById("login_button").click()
