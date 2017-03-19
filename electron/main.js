3fff02e5e827ccfe735693940ba218164c8ce800
ax4 <yaoyue718@gmail.com>
2017-03-19 19:32:10 +0800

    split the proxy

--- electron/main.js
+++ electron/main.js
@@ -28,7 +28,7 @@ function createWindow () {
   session = mainWindow.webContents.session
   console.log(session.getUserAgent())
   session.setProxy({
-    proxyRules : "http=127.0.0.1:8001;https=127.0.0.1:8001"
+    //proxyRules : "http=127.0.0.1:8001;https=127.0.0.1:8001"
   },()=>{
     console.log("Session set with proxy")
   })
@@ -72,24 +72,4 @@ app.on('activate', function () {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 
-// AnyProxy: module example from https://github.com/alibaba/anyproxy#work-as-a-module-for-nodejs
-var proxy = require("anyproxy");
-
-//create cert when you want to use https features
-//please manually trust this rootCA when it is the first time you run it
-!proxy.isRootCAFileExists() && proxy.generateRootCA();
-
-var options = {
-    type          : "http",
-    port          : 8001,
-    hostname      : "localhost",
-    rule          : require("./proxyRules.js"),
-    dbFile        : null,  // optional, save request data to a specified file, will use in-memory db if not specified
-    webPort       : 8002,  // optional, port for web interface
-    socketPort    : 8003,  // optional, internal port for web socket, replace this when it is conflict with your own service
-    //throttle      : 10,    // optional, speed limit in kb/s
-    disableWebInterface : false, //optional, set it when you don't want to use the web interface
-    setAsGlobalProxy : false, //set anyproxy as your system proxy
-    silent        : true //optional, do not print anything into terminal. do not set it when you are still debugging.
-};
-new proxy.proxyServer(options);
+//require('./proxyServer.js')
